e8dc754847571f120125c2626fd6860816a55893
LiS2Lim <s2alua.fu@gmail.com>
2023-08-07 21:27:01 +0900

    Q_11022

--- source/Q_11021.java
+++ source/Q_11021.java
@@ -12,6 +12,7 @@
  * 출력
  * 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
  */
+package source;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
