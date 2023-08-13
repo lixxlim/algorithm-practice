5eeceff702a51b5c37117a2d361a44af51753662
LiS2Lim <s2alua.fu@gmail.com>
2023-08-13 18:54:16 +0900

    Q_2738

---
+++ Lv1/Q_2738.java
@@ -0,0 +1,42 @@
+/**
+ * 문제
+ * N*M크기의 두 행렬 A와 B가 주어졌을 때, 두 행렬을 더하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 첫째 줄에 행렬의 크기 N 과 M이 주어진다. 둘째 줄부터 N개의 줄에 행렬 A의 원소 M개가 차례대로 주어진다. 이어서 N개의 줄에 행렬 B의 원소 M개가 차례대로 주어진다. N과 M은 100보다 작거나 같고, 행렬의 원소는 절댓값이 100보다 작거나 같은 정수이다.
+ * 
+ * 출력
+ * 첫째 줄부터 N개의 줄에 행렬 A와 B를 더한 행렬을 출력한다. 행렬의 각 원소는 공백으로 구분한다.
+ */
+package Lv1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q_2738 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
+
+            String[] nm = br.readLine().split(" ");
+            int n = Integer.parseInt(nm[0]);
+            int m = Integer.parseInt(nm[1]);
+
+            int[][] v = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < m; j++) {
+                    v[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < m; j++) {
+                    bw.write(v[i][j] + Integer.parseInt(st.nextToken()) + " ");
+                }
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (Exception e) {}
+    }
+}
