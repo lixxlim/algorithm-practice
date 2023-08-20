b8ec14c1743cd5aee4e2b6daf99a82e6c86eda97
LiS2Lim <s2alua.fu@gmail.com>
2023-08-20 21:37:45 +0900

    _2741

--- _Template.java
+++ _Template.java
@@ -1,28 +1,13 @@
 /**
  */
-package class1;
-
 import java.io.*;
 
 public class _Template {
   public static void main(String[] args) {
-    try(BufferedReader bw = new BufferedReader(new InputStreamReader(System.in))) {
-      String s = bw.readLine();
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+      String s = br.readLine();
 
     } catch (Exception e) { e.printStackTrace(); }
-  }
-
-  public String solution(String... s) {
-
-  }
-}
-
-class Test {
-  public static void main(String[] args) {
-    _Template t = new _Template();
-    String input = """
-        
-        """;
-    System.out.println(t.solution(input));
   }
 }
---
+++ class1/_2741.java
@@ -0,0 +1,26 @@
+/**
+ * 문제
+ * 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+ * 
+ * 출력
+ * 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+ */
+package class1;
+
+import java.io.*;
+
+public class _2741 {
+  public static void main(String[] args) {
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+      int s = Integer.parseInt(br.readLine());
+      for(int i = 1; i <= s; i++) {
+        bw.write(i + "\n");
+      }
+      bw.flush();
+    } catch (Exception e) {}
+  }
+}
