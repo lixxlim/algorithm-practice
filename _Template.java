9a51e98a0cb91951f82c41bed61471d5d6d94fd4
LiS2Lim <s2alua.fu@gmail.com>
2023-08-24 09:30:14 +0900

    _1181

--- _Template.java
+++ _Template.java
@@ -3,21 +3,18 @@
 import java.io.*;
 
 public class _Template {
-
   public static void main(String[] args) {
     try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
       int repeat = Integer.parseInt(br.readLine());
       String[] values = new String[repeat];
       for (int i = 0; i < repeat; i++) values[i] = br.readLine();
-
-      System.out.println(solution(values));
-
+      solution(values);
     } catch (Exception e) {}
   }
 
-  public static int solution(String[] values) {
+  public static void solution(String... values) {
 
-    return 0;
+    System.out.println(values);
   }
 
 }
