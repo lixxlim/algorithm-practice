21a03688e4fc25a34b8c44d06e5b90c298752297
LiS2Lim <s2alua.fu@gmail.com>
2023-08-27 18:28:47 +0900

    _10872

---
+++ Bronze/_10872.java
@@ -0,0 +1,25 @@
+/**
+ * 문제
+ * 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ * 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+ * 
+ * 출력
+ * 첫째 줄에 N!을 출력한다.
+ */
+package Bronze;
+
+import java.io.*;
+
+public class _10872 {
+  public static void main(String[] args) {
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      int n = Integer.parseInt(br.readLine());
+      int sum = 1;
+      for(int i = n; i > 0; i--) sum *= i;
+      System.out.println(sum);
+
+    } catch (Exception e) {}
+  }
+}
