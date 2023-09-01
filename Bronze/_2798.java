50e32003327c3c3aa96a59f9718ba6176a58d492
LiS2Lim <s2alua.fu@gmail.com>
2023-09-01 11:27:43 +0900

    _2798

---
+++ Bronze/_2798.java
@@ -0,0 +1,57 @@
+/**
+ * 문제
+ * 카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 블랙잭은 카지노마다 다양한 규정이 있다.
+ * 
+ * 한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+ * 
+ * 김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+ * 
+ * 이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+ * 
+ * N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+ * 
+ * 입력
+ * 첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+ * 
+ * 합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+ * 
+ * 출력
+ * 첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+ */
+package Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class _2798 {
+  public static void main(String[] args) {
+    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      st = new StringTokenizer(br.readLine());
+      int[] cards = new int[n];
+      for (int i = 0; i < n; i++) cards[i] = Integer.parseInt(st.nextToken());
+      long before = System.currentTimeMillis(); //TimeLoger
+      solution(n, m, cards);
+      System.out.println("소요시간: "+ ((System.currentTimeMillis()-before)/1000F) +" s"); //TimeLoger
+    } catch (Exception e) {} 
+
+  }
+
+  public static void solution(int n, int m, int[] cards) {
+    int result = 0;
+    repeat:
+    for(int c1 = 0; c1 < n; c1++) {
+      for(int c2 = c1+1; c2 < n; c2++) {
+        for(int c3 = c2+1; c3 < n; c3++) {
+          if(cards[c1]+cards[c2]+cards[c3] > m) continue; 
+          result = Math.max(result, cards[c1]+cards[c2]+cards[c3]);
+          if(result == m) break repeat;
+        }
+      }
+    }
+    System.out.println(result);
+  }
+
+}
