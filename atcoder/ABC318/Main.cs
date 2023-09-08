78321ab83e10d2c2e3de5c46cc68a9d5c6fdbc9c
s2alua <s2alua.fu@gmail.com>
2023-09-08 17:38:29 +0900

    Add gitignore

--- atcoder/ABC318/Main.cs
+++ atcoder/ABC318/Main.cs
@@ -5,7 +5,7 @@ public class Programs
 {
 	public static void Main(String[] args) 
 	{
-		A_FullMoon.Solution();
+		B_OverlappingSheets.Solution();
 	}
 }
 
@@ -22,5 +22,29 @@ class A_FullMoon
 		if (n[0] < n[1]) return 0;
     if (n[0] == n[1]) return 1;
     return ((n[0] - n[1]) / n[2]) + 1;
+	}
+}
+
+class B_OverlappingSheets
+{
+	static int[,] map = new int[101,101];
+	static int result = 0;
+
+	public static void Solution()
+	{
+		int loop = int.Parse(Console.ReadLine());
+		for(int l = 0; l < loop; l++)
+		{
+			int[] sq = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			for(int x = sq[0]; x < sq[1]; x++)
+			{
+				for(int y = sq[2]; y < sq[3]; y++)
+				{
+					if(map[x,y] == 0) result++;
+					map[x,y] = 1;
+				}
+			}
+		}
+		Console.WriteLine(result);
 	}
 }
