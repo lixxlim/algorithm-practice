969137fb1280a08fc21836e28a9f0bc4e963c4ca
LiS2Lim <s2alua.fu@gmail.com>
2023-09-13 14:22:04 +0900

    B_CanYouSolveThis

---
+++ atcoder/src/B_CanYouSolveThis.cs
@@ -0,0 +1,26 @@
+// https://atcoder.jp/contests/abc121/tasks/abc121_b
+
+int[] nmc = Console.ReadLine()
+                   .Split()
+                   .Select(int.Parse)
+                   .ToArray();
+int[] bnm = Console.ReadLine()
+                   .Split()
+                   .Select(int.Parse)
+                   .ToArray();
+int result = 0;
+for(int n = 0; n < nmc[0]; n++)
+{
+    int[] anm = Console.ReadLine()
+                       .Split()
+                       .Select(int.Parse)
+                       .ToArray();
+    int sum = nmc[2];
+    for(int m =  0; m < nmc[1]; m++)
+    {
+        sum += bnm[m] * anm[m];
+    }
+    if(sum > 0) { result++; }
+
+}
+Console.WriteLine(result);
