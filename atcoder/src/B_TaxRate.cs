8936abf4d59db91d93c836a6884989358b12cfc4
LiS2Lim <s2alua.fu@gmail.com>
2023-09-13 10:15:20 +0900

    B_TaxRate

---
+++ atcoder/src/B_TaxRate.cs
@@ -0,0 +1,20 @@
+﻿//  https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_b
+#nullable disable
+
+class B_TaxRate
+{
+	static void Main()
+	{
+		Console.WriteLine(Solution());
+	}
+
+	static string Solution()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int x = (int)Math.Round(n / 1.08);
+		if(x + (int)(x * 0.08) == n) return x.ToString();
+		if((x-1) + (int)((x-1) * 0.08) == n) return (x-1).ToString();
+		if((x+1) + (int)((x+1) * 0.08) == n) return (x+1).ToString();
+		return ":(";
+	}
+}
