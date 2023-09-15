0567b5cb80dc27a01f4b313037ced3c51dc7f185
LiS2Lim <s2alua.fu@gmail.com>
2023-09-15 11:13:00 +0900

    B_CollectingBalls

---
+++ atcoder/src/B_CollectingBalls.cs
@@ -0,0 +1,25 @@
+// https://atcoder.jp/contests/abc074/tasks/abc074_b
+#nullable disable
+
+class B_CollectingBalls
+{
+	static void Main()
+	{
+		Solution();
+	}
+
+	static void Solution()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int k = int.Parse(Console.ReadLine());
+		int[] x = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int result = 0;
+        foreach(int i in x) 
+        {
+            if (i > (k - i)) result += (k - i) << 1;
+            else result += i << 1;
+        }
+		Console.WriteLine(result);
+	}
+}
