64f93bad877454f55c2c7dbd2b7bee83389355f8
LiS2Lim <s2alua.fu@gmail.com>
2023-09-22 10:57:09 +0900

    DivideTheProblems

---
+++ atcoder/src/C_DivideTheProblems.cs
@@ -0,0 +1,24 @@
+// https://atcoder.jp/contests/abc132/tasks/abc132_c
+// 00:10:23
+#nullable disable
+
+class DivideTheProblems
+{
+	static void Main()
+	{
+        Console.WriteLine("--- Started ---");
+		Solution();
+        Console.WriteLine("--- Finished ---");
+	}
+
+	static void Solution()
+	{
+		int mid = int.Parse(Console.ReadLine()) / 2;
+        int[] tests = Console.ReadLine()
+                             .Split()
+                             .Select(int.Parse)
+                             .Order()
+                             .ToArray();
+		Console.WriteLine(tests[mid] - tests[mid-1]);
+	}
+}
