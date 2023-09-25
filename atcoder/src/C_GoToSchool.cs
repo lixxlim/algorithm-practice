0a4c43a3934e24577dc7d4c1da90bc4180ea3a93
LiS2Lim <s2alua.fu@gmail.com>
2023-09-25 09:10:34 +0900

    GoToSchool

---
+++ atcoder/src/C_GoToSchool.cs
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc142/tasks/abc142_c
+#nullable disable
+
+class GoToSchool
+{
+	static void Main()
+	{
+        Console.WriteLine("--- Started ---");
+		Solution();
+        Console.WriteLine("--- Finished ---");
+	}
+
+	static void Solution()
+	{
+		int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] attend = new int[n];
+        for(int i = 0; i < n; i++) { attend[a[i]-1] = i+1; }
+        foreach(int i in attend) { Console.WriteLine(i + " "); }
+	}
+}
