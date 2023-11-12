12f92c2f5c5d543103aca475d4897c19fcb0ca20
LiS2Lim <s2alua.fu@gmail.com>
2023-11-12 23:15:46 +0900

    S3_3182

--- backjoon_solvedac/Graph/S3_3182.java
+++ backjoon_solvedac/Graph/S3_3182.java
@@ -2,27 +2,37 @@ package backjoon_solvedac.Graph;
 // https://www.acmicpc.net/problem/3182
 // 00:00:00
 import java.io.*;
-import java.util.*;
 
 public class S3_3182 {
 	public static void main(String[] args) throws Exception {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             // input
 			int n = Integer.parseInt(br.readLine());
-            int[] graph = new int[n];
-            for(int i = 0; i < n; i++) {
-                graph[i] = Integer.parseInt(br.readLine());
+            int[] senpai = new int[n+1];
+            for(int i = 1; i <= n; i++) {
+                senpai[i] = Integer.parseInt(br.readLine());
             }
 
             // process
-            int result = solution();
+            int max = 0;
+            int result = 0;
+            for(int i = 1; i <= n; i++) {
+                int num = solution(senpai.clone(), 1, i);
+                if(max < num) {
+                    result = i;
+                    max = num;
+                }
+            }
 
             // output
             System.out.println(result);
 		}
 	}
 
-	public static void solution(String s) {
-		System.out.println(s);
+	public static int solution(int[] senpai, int num, int prev) {
+        if(senpai[prev] == -1) return num;
+        int next = senpai[prev];
+        senpai[prev] = -1;
+        return solution(senpai, num+1, next);
 	}
 }
