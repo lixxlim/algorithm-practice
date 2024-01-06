d242c62b188a714166c66d58496cb76d66178960
LiS2Lim <s2alua.fu@gmail.com>
2024-01-06 22:38:13 +0900

    B5_21598

---
+++ clang/B5_21598.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/21598
+
+#include <stdio.h>
+
+int main() {
+	int input;
+	scanf("%d", &input);
+	for(int i = 0; i < input; i++)
+	{
+		printf("SciComLove\n");
+	}
+	return 0;
+}
