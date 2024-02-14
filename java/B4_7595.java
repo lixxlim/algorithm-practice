b784a96e7aed182b4dbc608de5a8c2f8eef2626c
LiS2Lim <s2alua.fu@gmail.com>
2024-02-14 16:30:42 +0900

    B4_7595.java

---
+++ java/B4_7595.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int n = 0;
+			while((n = Integer.parseInt(br.readLine())) != 0) {
+				for(int i = 1; i <= n; i++) {
+					System.out.println("*".repeat(i));
+				}
+			}
+		}
+	}
+}
