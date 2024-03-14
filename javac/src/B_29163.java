1df99539b96bae562861b9dbf76d2bbccf805d89
LiS2Lim <s2alua.fu@gmail.com>
2024-03-14 09:43:08 +0900

    B_29163.java

---
+++ javac/src/B_29163.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class B_29163 {
+    public static void main(String[] args) throws IOException {
+        try(var br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            int chk = Arrays.stream(br.readLine().split(" "))
+                            .mapToInt(Integer::parseInt)
+                            .map(v -> v & 1)
+                            .sum();
+            System.out.println(n-chk > chk ? "Happy" : "Sad");
+        }
+    }
+}
