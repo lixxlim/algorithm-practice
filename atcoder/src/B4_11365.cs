5ded539cb7337fd4a60e42787ac158bf9351c53c
LiS2Lim <s2alua.fu@gmail.com>
2024-04-08 22:52:58 +0900

    B4_11365.cs

---
+++ atcoder/src/B4_11365.cs
@@ -0,0 +1,31 @@
+﻿// https://www.acmicpc.net/problem/11365
+// 00:00:00
+#nullable disable
+
+using System.Text;
+
+class B4_11365
+{
+	/*
+	static void Main()
+	{
+        Console.WriteLine("--- Started ---");
+		Solution();
+        Console.WriteLine("--- Finished ---");
+	}
+	*/
+
+	static void Solution()
+	{
+		var sb = new StringBuilder();
+		while (true)
+		{
+			var str = Console.ReadLine();
+			if ("END".Equals(str)) break;
+			char[] chars = str.ToCharArray();
+			Array.Reverse(chars);
+	        sb.Append(chars).Append('\n');
+		}
+		Console.WriteLine(sb);
+	}
+}
