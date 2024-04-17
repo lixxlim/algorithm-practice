f66ec5f46df261223faed6df032b2b3d88a8eb35
LiS2Lim <s2alua.fu@gmail.com>
2024-04-17 09:44:38 +0900

    B4_11549.cs

---
+++ atcoder/src/B4_11549.cs
@@ -0,0 +1,19 @@
+﻿// https://www.acmicpc.net/problem/11549
+#nullable disable
+
+class B4_11549
+{
+	//static void Main()
+	//{
+	//	Console.WriteLine("--- Started ---");
+	//	Solution();
+	//	Console.WriteLine("--- Finished ---");
+	//}
+
+	static void Solution()
+	{
+		int t = int.Parse(Console.ReadLine());
+        int res = Console.ReadLine().Split().Select(int.Parse).Where(n => n == t).Count();
+		Console.WriteLine(res);
+	}
+}
