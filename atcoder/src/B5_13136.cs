41006a7add09b6260c135fc8cfcb6064f10b70eb
LiS2Lim <s2alua.fu@gmail.com>
2024-04-20 11:05:51 +0900

    B5_13136.cs

---
+++ atcoder/src/B5_13136.cs
@@ -0,0 +1,18 @@
+﻿// https://www.acmicpc.net/problem/13136
+#nullable disable
+
+class B_13136
+{
+	static void Main()
+	{
+		Console.WriteLine("--- Started ---");
+		Solution();
+		Console.WriteLine("--- Finished ---");
+	}
+
+	static void Solution()
+	{
+		long[] v = Console.ReadLine().Split().Select(long.Parse).ToArray();
+		Console.WriteLine(((v[0] - 1) / v[2] + 1) * ((v[1] - 1)/ v[2] + 1));
+	}
+}
