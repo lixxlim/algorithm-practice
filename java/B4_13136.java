8e9ba2fdbbf672ff105d7b7c635718ed3f0eb4b9
LiS2Lim <s2alua.fu@gmail.com>
2024-04-20 11:14:18 +0900

    B4_13136.java

---
+++ java/B4_13136.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		try(var br = new BufferedReader(new InputStreamReader(System.in))) {
+			long[] RCN = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+			System.out.println(
+				((RCN[0]-1)/RCN[2]+1) * ((RCN[1]-1)/RCN[2]+1)
+			);
+		}
+	}
+}
