dd3366a355966092e55aa1f031f0071e267d0459
LiS2Lim <s2alua.fu@gmail.com>
2024-04-28 22:50:41 +0900

    B4_15700.c

---
+++ clang/B4_15700.c
@@ -0,0 +1,10 @@
+//// https://www.acmicpc.net/problem/15700
+#include <stdio.h>
+
+int main(void)
+{
+	long long N = 0, M = 0;
+	scanf("%ld %ld", &N, &M);
+	printf("%ld\n", N * M / 2);
+	return 0;
+}
