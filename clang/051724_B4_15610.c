70b10c0deabfe91a305f3d609bd91c49d671aaaf
LiS2Lim <s2alua.fu@gmail.com>
2024-05-17 08:47:05 +0900

    B4_15610.c

---
+++ clang/051724_B4_15610.c
@@ -0,0 +1,13 @@
+/*
+ * https://www.acmicpc.net/problem/15610
+ */
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	long long yard;
+	scanf("%lld", &yard);
+	printf("%lf\n", sqrt(yard) * 4);
+	return 0;
+}
