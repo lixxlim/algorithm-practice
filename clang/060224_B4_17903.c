7f4bc91313adecd9bd364fcea8f32f9442d4bf7a
LiS2Lim <s2alua.fu@gmail.com>
2024-06-02 20:31:26 +0900

    B4_17903.c

---
+++ clang/060224_B4_17903.c
@@ -0,0 +1,13 @@
+/*
+ * https://www.acmicpc.net/problem/17903
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	puts(n >= 8 ? "satisfactory" : "unsatisfactory");
+	return EXIT_SUCCESS;
+}
