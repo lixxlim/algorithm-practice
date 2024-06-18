b223e51a6d96a1345bf5c10083d7ecec7c9cf775
LiS2Lim <s2alua.fu@gmail.com>
2024-06-19 08:57:07 +0900

    B5_20232.c

---
+++ clang/061924_B5_20232.c
@@ -0,0 +1,50 @@
+/*
+ * https://www.acmicpc.net/problem/20232
+ */
+#include <stdio.h>
+
+typedef struct {
+	int year;
+	char name[13];
+} Winner;
+
+Winner winners[] = {
+	{ 1995, "ITMO" },
+	{ 1996, "SPbSU" },
+	{ 1997, "SPbSU" },
+	{ 1998, "ITMO" },
+	{ 1999, "ITMO" },
+	{ 2000, "SPbSU" },
+	{ 2001, "ITMO" },
+	{ 2002, "ITMO" },
+	{ 2003, "ITMO" },
+	{ 2004, "ITMO" },
+	{ 2005, "ITMO" },
+	{ 2006, "PetrSU, ITMO" },
+	{ 2007, "SPbSU" },
+	{ 2008, "SPbSU" },
+	{ 2009, "ITMO" },
+	{ 2010, "ITMO" },
+	{ 2011, "ITMO" },
+	{ 2012, "ITMO" },
+	{ 2013, "SPbSU" },
+	{ 2014, "ITMO" },
+	{ 2015, "ITMO" },
+	{ 2016, "ITMO" },
+	{ 2017, "ITMO" },
+	{ 2018, "SPbSU" },
+	{ 2019, "ITMO" },
+};
+
+int main( void )
+{
+	int inp, i;
+	scanf( "%d", &inp );
+	for( i = 0; i < sizeof(winners) / sizeof(Winner); i++ ) {
+		if( winners[ i ].year == inp ) {
+			puts( winners[ i ].name );
+			break;
+		}
+	}
+	return 0;
+}
