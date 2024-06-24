9a3d62df1f8307e0732b695e9b5a94d1e563278b
LiS2Lim <s2alua.fu@gmail.com>
2024-06-24 10:01:45 +0900

    S5_1402.c

---
+++ clang/062424_S5_1402.c
@@ -0,0 +1,17 @@
+/*
+ * https://www.acmicpc.net/problem/1402
+ */
+#include <stdio.h>
+
+int main( void )
+{
+	int N;
+	int A, B;
+	scanf( "%d", &N );
+	while( N-- )
+	{
+		scanf( "%d %d", &A, &B );
+		puts("yes");
+	}
+	return 0;
+}
