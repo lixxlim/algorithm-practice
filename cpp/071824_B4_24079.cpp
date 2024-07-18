6b82b3a372ce6d9e3df598d17b8f717a7699e1dd
LiS2Lim <s2alua.fu@gmail.com>
2024-07-18 09:17:52 +0900

    B4_24079.cpp

---
+++ cpp/071824_B4_24079.cpp
@@ -0,0 +1,12 @@
+/**
+ * https://www.acmicpc.net/problem/24079
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, z;
+	
+	cin >> x >> y >> z;
+	cout << ( x + y < z + 1 ) << endl;
+}
