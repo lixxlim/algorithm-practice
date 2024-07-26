e5228a115ed0e89f3adc8f182db91a871ba7d7e3
LiS2Lim <s2alua.fu@gmail.com>
2024-07-26 09:31:59 +0900

    B4_24751.cpp

---
+++ cpp/072624_B4_24751.cpp
@@ -0,0 +1,11 @@
+/**
+ * https://www.acmicpc.net/problem/24751
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << 100.0/n << "\n" << 100.0/(100-n) << endl;
+}
