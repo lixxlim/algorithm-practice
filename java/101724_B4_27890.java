ed12725edfa5fc742b63a6ab27311437ba331f4d
LiS2Lim <s2alua.fu@gmail.com>
2024-10-17 19:20:24 +0900

    101724_B4_27890.java

---
+++ java/101724_B4_27890.java
@@ -0,0 +1,24 @@
+/**
+ * https://www.acmicpc.net/problem/27890
+ */
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        for(int i=0;i<N;i++){
+            if(X%2==0){
+                X=(X/2)^6;
+            }else{
+                X=(2*X)^6;
+            }
+        }
+
+        System.out.println(X);
+        scanner.close();
+    }
+}
