5cc5a72d10aec60682c34826671b4b8e7625fe91
lixlim <s2alua.fu@gmail.com>
2024-10-19 11:17:51 +0900

    101524_S5_3845.java

--- java/101524_S5_3845.java
+++ java/101524_S5_3845.java
@@ -5,70 +5,91 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        try(var br = new BufferedReader(new InputStreamReader(System.in))) {
-            // variables
-            Boolean[][] map= new Boolean[75][100];
-            String str;
-            StringTokenizer st;
-            StringTokenizer xi;
-            StringTokenizer yi;
-            int i, j;
-            int nx, ny;
-            double w, x, y, start, end;
-            
 
-            // input
-            str = br.readLine();
-            
-            // solution
-            while(!str.equals("0 0 0.0")) {
-                
-                // preprocess
-                st = new StringTokenizer(str);
-                nx = Integer.parseInt(st.nextToken());
-                ny = Integer.parseInt(st.nextToken());
-                w = Double.parseDouble(st.nextToken());
-                xi = new StringTokenizer(br.readLine());
-                yi = new StringTokenizer(br.readLine());
-                
-                // x
-                while(xi.hasMoreTokens()) {
-                    x = Integer.parseInt(xi.nextToken());
-                    start = x - (x / 2) < 0 ? 0 : x - (x / 2);
-                    end = x + (x / 2) + 1 > 75 ? 75 : x + (x / 2) + 1;
-                    for(i = (int)start; i < end; i++) {
-                        for(j = 0; j < ny; j++) {
-                            map[i][j] = true;
-                        }
-                    }
-                }
-
-                // y
-                while(yi.hasMoreTokens()) {
-                    y = Integer.parseInt(yi.nextToken());
-                    start = y - (y / 2) < 0 ? 0 : y - (y / 2);
-                    end = y + (y / 2) + 1 > 100 ? 100 : y + (y / 2) + 1;
-                    for(i = (int)start; i < end; i++) {
-                        for(j = 0; j < ny; j++) {
-                            map[j][i] = true;
-                        }
-                    }
-                }
-
-                // input next
-                str = br.readLine();
-            }
-
-            // output
-            System.out.println(
-                Arrays.stream(map)
-                    .flatMap(Arrays::stream)
-                    .filter(v -> !v)
-                    .findAny()
-                    .orElse(true)
-                    ? "NO" : "YES"
-            );
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		String s = "";
+		
+		int nx, ny;
+		double w, now;
+		boolean check;
+		List<Double> arr;
+		StringTokenizer st;
+		
+		// "0 0 0.0"이 들어오면 반복문을 종료한다.
+		while (!(s = br.readLine()).equals("0 0 0.0")) {
+			st = new StringTokenizer(s);
+			nx = Integer.parseInt(st.nextToken()); // 가로를 평행하게 깍은 횟수
+			ny = Integer.parseInt(st.nextToken()); // 세로를 평행하게 깍은 횟수
+			w = Double.parseDouble(st.nextToken()); // 폭
+			
+			check = true;
+			arr = new LinkedList<>();
+			
+			// x좌표 먼저 확인.
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < nx; i++) {
+				arr.add(Double.parseDouble(st.nextToken()));
+			}
+			
+			// 앞에서 부터 잔디 정리를 위해 정렬
+			Collections.sort(arr);
+			
+			// 현재 까지 잔디 정렬을 한 위치
+			now = 0;
+			for (int i = 0; i < nx; i++) {
+				// 현재 까지 잔디 정리를 한 위치에서 연속적으로 연결이 된다면
+				if (arr.get(i) - (w / 2) <= now) {
+					now = arr.get(i) + (w / 2);
+				} else { // 중간에 빈 공간이 있다면
+					check = false;
+					break;
+				}
+			}
+			
+			// 모든 면적을 하지 못했거나, 중간에 빈 공간이 있다면
+			if (!check || now < 75) {
+				br.readLine();
+				sb.append("NO\n");
+				continue;
+			}
+			
+			arr = new LinkedList<>();
+			
+			// y좌표 계산
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < ny; i++) {
+				arr.add(Double.parseDouble(st.nextToken()));
+			}
+			
+			// 아래에서 부터 위로 잔디 정리를 위해 정렬
+			Collections.sort(arr);
+			
+			// 현재까지 정리한 위치
+			now = 0;
+			for (int i = 0; i < ny; i++) {
+				// 현재 까지 잔디 정리를 한 위치에서 연속적으로 연결이 된다면
+				if (arr.get(i) - (w / 2) <= now) {
+					now = arr.get(i) + (w / 2);
+				} else { // 중간에 빈 공간이 있다면
+					check = false;
+					break;
+				}
+			}
+			
+			// 모든 면적을 하지 못했거나, 중간에 빈 공간이 있다면
+			if (!check || now < 100) {
+				sb.append("NO\n");
+			} else {
+				sb.append("YES\n");
+			}
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
