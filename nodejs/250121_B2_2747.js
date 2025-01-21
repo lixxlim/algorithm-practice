fc7a02eac1e7f960f395b50b514d9f81184d3033
lixlim <s2alua.fu@gmail.com>
2025-01-21 13:39:23 +0900

    250121_B2_2747.js

---
+++ nodejs/250121_B2_2747.js
@@ -0,0 +1,10 @@
+/**
+ * https://www.acmicpc.net/problem/2747
+ */
+const n = Number(require('fs').readFileSync(0, 'utf-8').toString().trim());
+const dp = new Array(n+1).fill(0);
+dp[1] = 1;
+for(let i = 2; i <= n; i++) {
+	dp[i] = dp[i-1] + dp[i-2];
+}
+console.log(dp[n]);
