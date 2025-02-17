f4552ae7f5bed4d00c9c58c2f950a6dc70ff3302
lixlim <s2alua.fu@gmail.com>
2025-02-18 08:25:39 +0900

    250218_B3_24266.c

---
+++ clang/250218_B3_24266.c
@@ -0,0 +1,12 @@
+/**
+ * https://www.acmicpc.net/problem/24266
+ */
+#include <stdio.h>
+
+int main( void )
+{
+	long long n;
+	scanf( "%lld", &n );
+	printf( "%lld\n\%d\n", n * n * n, 3 );
+	return 0;
+}
