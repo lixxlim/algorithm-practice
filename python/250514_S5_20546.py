29f4054b471df0b4fe3ace170abddf6083fa8e74
lixlim <s2alua.fu@gmail.com>
2025-05-14 23:59:46 +0900

    250514_S5_20546.py

---
+++ python/250514_S5_20546.py
@@ -0,0 +1,70 @@
+# https://www.acmicpc.net/problem/20546
+from enum import Enum
+
+class Deal(Enum):
+    BUY = 0
+    SELL = 1
+    WAIT = 2
+
+class Stratege(Enum):
+    BNF = 0
+    TIMING = 1
+
+class Person:
+    def __init__(self, stratege, money):
+        self.stratege = stratege
+        self.money = money
+        self.stock = 0
+        self.before_price = 0
+        self.move_history = 0
+
+    def do_stratege(self, price):
+        # 행동 결정
+        deal = Deal.WAIT
+        if self.stratege == Stratege.BNF:
+            # BNF는 항상 구매
+            deal = Deal.BUY
+        elif self.stratege == Stratege.TIMING:
+            # 가격변동 기록
+            if self.before_price < price:
+                self.move_history = max(1, self.move_history + 1)
+            elif self.before_price > price:
+                self.move_history -= min(-1, self.move_history - 1)
+            else:
+                self.move_history = 0
+            # 가격변동에 따른 행동 결정
+            if self.move_history >= 3:
+                deal = Deal.SELL
+            elif self.move_history <= 3:
+                deal = Deal.BUY
+        # 결정된 행동 실행
+        if deal == Deal.BUY:
+            self.stock += self.money // price
+            self.money = self.money % price
+        elif deal == Deal.SELL:
+            self.money += self.stock * price
+            self.stock = 0
+        # 전날 가격 기록
+        self.before_price = price
+
+    def get_result(self):
+        self.money += self.before_price * self.stock
+        self.stock = 0
+        return self.money
+
+if __name__ == "__main__":
+    money = int(input())
+    jun_hyon = Person(Stratege.BNF, money)
+    sung_min = Person(Stratege.TIMING, money)
+    machine_duck = map(int, input().split())
+    for price in machine_duck:
+        jun_hyon.do_stratege(price)
+        sung_min.do_stratege(price)
+    if jun_hyon.get_result() > sung_min.get_result():
+        print("jun", jun_hyon.money, "sung", sung_min.money)
+        print(jun_hyon.stratege.name)
+    elif jun_hyon.get_result() < sung_min.get_result():
+        print("jun", jun_hyon.money, "sung", sung_min.money)
+        print(sung_min.stratege.name)
+    else:
+        print("SAMESAME")
