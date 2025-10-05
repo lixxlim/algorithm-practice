284e25dfd0117233f22595850e8d3b29cc097eb1
lixlim <s2alua.fu@gmail.com>
2025-10-05 23:33:41 +0900

    251005_B1_2163.cpp

---
+++ cpp/251005_B1_2163.cpp
@@ -0,0 +1,13 @@
+/**
+ * https://www.acmicpc.net/problem/2163
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    cout << N * M - 1 << endl;
+    return 0;
+}
+
